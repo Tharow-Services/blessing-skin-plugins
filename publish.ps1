97c2f2c47270cdcba0f64008bb76fbe66ba127be
Theray Tharow <80534152+TharowT@users.noreply.github.com>
2022-10-26 07:33:44 -0400

    Update Run Things

--- publish.ps1
+++ publish.ps1
@@ -2,8 +2,8 @@ if (!(Test-Path updated.json)) {
     exit
 }
 
-git config --global user.name 'Pig Fang'
-git config --global user.email 'g-plane@hotmail.com'
+git config --global user.name 'Theray Tharow - Bot'
+git config --global user.email 'tharowt@tharow.net'
 
 $token = $env:GH_TOKEN
 
@@ -17,16 +17,4 @@ if ($shouldUpdate) {
   git push origin master
 }
 
-Set-Location '..'
 
-$botRelease = (Invoke-WebRequest 'https://api.github.com/repos/bs-community/telegram-bot/releases/latest').Content | ConvertFrom-Json
-$botBinUrl = ((Invoke-WebRequest $botRelease.assets_url).Content | ConvertFrom-Json).browser_download_url
-
-bash -c "curl -fSL $botBinUrl -o bot"
-chmod +x ./bot
-./bot plugin updated.json
-
-foreach ($lang in 'en', 'zh_CN') {
-    Invoke-WebRequest "https://purge.jsdelivr.net/gh/bs-community/plugins-dist@latest/registry_$lang.json"
-}
-Invoke-WebRequest 'https://purge.jsdelivr.net/gh/bs-community/plugins-dist@latest/registry.json'
