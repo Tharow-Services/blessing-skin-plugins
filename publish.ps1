0d46eac8c8faee08eab334e14b9067c66d111cdc
Theray Tharow <80534152+TharowT@users.noreply.github.com>
2022-11-03 10:15:54 -0400

    push

--- publish.ps1
+++ publish.ps1
@@ -8,12 +8,12 @@ git config --global user.email 'tharowt@tharow.net'
 $token = $env:GH_TOKEN
 
 Set-Location .dist
-git add .
+git add . -f
 
 $shouldUpdate = git status -s
 if ($shouldUpdate) {
   git commit -m "Publish"
-  git remote set-url origin "https://tharowt:$token@github.com/Tharow-Services/plugins-dist.git"
+  git remote set-url origin "https://$token@github.com/Tharow-Services/plugins-dist.git"
   git push origin master
 }
 
